06955f0eb2f6199667a0312f0da359ff3b6adb8a
himzz1234 <himanshuhim1230@gmail.com>
2025-08-21 19:06:49 +0530

    chore: app, main, and styles update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import "./App.css";
-import Feature from "./components/Feature";
-import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import Faqs from "./components/landing/Faqs";
+import Feature from "./components/landing/Feature";
+import Hero from "./components/landing/Hero";
+import LearningPath from "./components/landing/LearningPath";
+import ReadySection from "./components/landing/ReadySection";
+import Testimonials from "./components/landing/Testimonials";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -13,7 +18,41 @@ function App() {
       </section>
 
       <section>
+        <main className="relative max-w-4xl mx-auto pb-16">
+          <img
+            src="/images/demo-screenshot.png"
+            className="w-full h-auto shadow-xl relative z-10"
+          />
+          <div
+            className="absolute bottom-0 left-0 w-full h-80 
+                 bg-gradient-to-t from-[#000000] from-20% to-transparent 
+                 z-20"
+          />
+        </main>
+      </section>
+
+      <section>
+        <LearningPath />
+      </section>
+
+      <section>
         <Feature />
+      </section>
+
+      <section>
+        <Testimonials />
+      </section>
+
+      <section>
+        <Faqs />
+      </section>
+
+      <section>
+        <ReadySection />
+      </section>
+
+      <section>
+        <Footer />
       </section>
     </main>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SortingPage from "./pages/sorting/index.tsx";
 import SearchingPage from "./pages/searching/index.tsx";
+import PathfindingPage from "./pages/pathfinding/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />} />
         <Route path="/sorting" element={<SortingPage />} />
         <Route path="/searching" element={<SearchingPage />} />
+        <Route path="/pathfinding" element={<PathfindingPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
