dedb9a08ec2ff492c8843fa1418faefb9b314a31
himzz1234 <himanshuhim1230@gmail.com>
2025-09-01 22:37:11 +0530

    Update main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -7,17 +6,28 @@ import SortingPage from "./pages/sorting/index.tsx";
 import SearchingPage from "./pages/searching/index.tsx";
 import PathfindingPage from "./pages/pathfinding/index.tsx";
 import ScrollToTop from "./components/ScrollToTop.tsx";
+import ModalProvider from "./context/ModalContext.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
+import LearnLayout from "./components/layout/LearnLayout.tsx";
+import LearningOverview from "./pages/learn/index.tsx";
+import ArrayPage from "./pages/array/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <AuthProvider>
     <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/sorting" element={<SortingPage />} />
-        <Route path="/searching" element={<SearchingPage />} />
-        <Route path="/pathfinding" element={<PathfindingPage />} />
-      </Routes>
+      <ModalProvider>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/learn" element={<LearnLayout />}>
+            <Route index element={<LearningOverview />} />
+            <Route path="array" element={<ArrayPage />} />
+            <Route path="sorting" element={<SortingPage />} />
+            <Route path="searching" element={<SearchingPage />} />
+            <Route path="pathfinding" element={<PathfindingPage />} />
+          </Route>
+        </Routes>
+      </ModalProvider>
     </BrowserRouter>
-  </StrictMode>
+  </AuthProvider>
 );
