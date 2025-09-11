fba3422dd2988ad19c34a576e1148ebe1957def6
himzz1234 <himanshuhim1230@gmail.com>
2025-09-11 11:38:03 +0530

    updated main file

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SortingPage from "./pages/sorting/index.tsx";
-import SearchingPage from "./pages/searching/index.tsx";
-import PathfindingPage from "./pages/pathfinding/index.tsx";
+import SortingPage from "./pages/learn/sorting/index.tsx";
+import SearchingPage from "./pages/learn/searching/index.tsx";
+import PathfindingPage from "./pages/learn/pathfinding/index.tsx";
 import ScrollToTop from "./components/ScrollToTop.tsx";
 import ModalProvider from "./context/ModalContext.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import LearnLayout from "./components/layout/LearnLayout.tsx";
 import LearningOverview from "./pages/learn/index.tsx";
-import ArrayPage from "./pages/array/index.tsx";
+import ArrayPage from "./pages/learn/array/index.tsx";
+import ListPage from "./pages/learn/list/index.tsx";
+import ExplorePage from "./pages/explore/index.tsx";
+import Dashboard from "./pages/user/[slug]/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
@@ -24,8 +27,11 @@ createRoot(document.getElementById("root")!).render(
             <Route path="array" element={<ArrayPage />} />
             <Route path="sorting" element={<SortingPage />} />
             <Route path="searching" element={<SearchingPage />} />
+            <Route path="list" element={<ListPage />} />
             <Route path="pathfinding" element={<PathfindingPage />} />
           </Route>
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/user/:id" element={<Dashboard />} />
         </Routes>
       </ModalProvider>
     </BrowserRouter>
