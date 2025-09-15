32b7d349104cae278732fcef4605c8f669a3ca00
himzz1234 <himanshuhim1230@gmail.com>
2025-09-15 14:42:59 +0530

    feat(analytics): vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -14,27 +14,31 @@ import ListPage from "./pages/learn/list/index.tsx";
 import ExplorePage from "./pages/explore/index.tsx";
 import Dashboard from "./pages/user/[slug]/index.tsx";
 import { ProgressProvider } from "./context/ProgressContext.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <BrowserRouter>
+  <>
+    <AuthProvider>
       <ProgressProvider>
         <ModalProvider>
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/learn" element={<LearnLayout />}>
-              <Route index element={<LearningOverview />} />
-              <Route path="array" element={<ArrayPage />} />
-              <Route path="sorting" element={<SortingPage />} />
-              <Route path="searching" element={<SearchingPage />} />
-              <Route path="list" element={<ListPage />} />
-            </Route>
-            <Route path="/explore" element={<ExplorePage />} />
-            <Route path="/user/:id" element={<Dashboard />} />
-          </Routes>
+          <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/learn" element={<LearnLayout />}>
+                <Route index element={<LearningOverview />} />
+                <Route path="array" element={<ArrayPage />} />
+                <Route path="sorting" element={<SortingPage />} />
+                <Route path="searching" element={<SearchingPage />} />
+                <Route path="list" element={<ListPage />} />
+              </Route>
+              <Route path="/explore" element={<ExplorePage />} />
+              <Route path="/user/:id" element={<Dashboard />} />
+            </Routes>
+          </BrowserRouter>
         </ModalProvider>
       </ProgressProvider>
-    </BrowserRouter>
-  </AuthProvider>
+    </AuthProvider>
+    <Analytics />
+  </>
 );
